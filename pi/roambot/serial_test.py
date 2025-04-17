6480d271dc595178c6c19b4effc7d3d89dfe8bcd
mannndrew <mannndrew@gmail.com>
2025-04-17 03:34:36 -0500

    Update serial_test.py

--- pi/roambot/serial_test.py
+++ pi/roambot/serial_test.py
@@ -1,5 +1,24 @@
+######################################################################
+## Instructions
+######################################################################
+
+##### SETUP #####
+# ls /dev/tty*
+# sudo nano /boot/firmware/config.txt
+# enable_uart=1
+# dtoverlay=disable-bt
+# sudo reboot
+
+######################################################################
+## Imports
+######################################################################
+
 import serial
 import time
+
+######################################################################
+## Main Program
+######################################################################
 
 # Open serial port (adjust tty name and baudrate as needed)
 ser = serial.Serial('/dev/ttyS0', 9600)  # or /dev/ttyAMA0 or /dev/ttyUSB0
